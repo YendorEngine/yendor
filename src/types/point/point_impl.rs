5429641bffe6cc3e9b3f59e24e166995657bf076
Jacob LeCoq <lecoqjacob@gmail.com>
2022-12-09 14:54:19 -0600

    fix point impl

--- src/types/point/point_impl.rs
+++ src/types/point/point_impl.rs
@@ -3,9 +3,29 @@ use crate::prelude::*;
 macro_rules! impl_grid_point_array {
     ($type:ty) => {
         impl Point for $type {
-            fn x(&self) -> i32 { self[0] as i32 }
+            fn x_int32(&self) -> i32 {
+                self.x as i32
+            }
 
-            fn y(&self) -> i32 { self[1] as i32 }
+            fn y_int32(&self) -> i32 {
+                self.y as i32
+            }
+
+            fn x_uint32(&self) -> u32 {
+                self.x as u32
+            }
+
+            fn y_uint32(&self) -> u32 {
+                self.y as u32
+            }
+
+            fn x_float32(&self) -> f32 {
+                self.x as f32
+            }
+
+            fn y_float32(&self) -> f32 {
+                self.y as f32
+            }
         }
     };
 }
@@ -13,9 +33,29 @@ macro_rules! impl_grid_point_array {
 macro_rules! impl_grid_point_tuple {
     ($type:ty) => {
         impl Point for $type {
-            fn x(&self) -> i32 { self.0 as i32 }
+            fn x_int32(&self) -> i32 {
+                self.0 as i32
+            }
 
-            fn y(&self) -> i32 { self.1 as i32 }
+            fn y_int32(&self) -> i32 {
+                self.1 as i32
+            }
+
+            fn x_uint32(&self) -> u32 {
+                self.0 as u32
+            }
+
+            fn y_uint32(&self) -> u32 {
+                self.1 as u32
+            }
+
+            fn x_float32(&self) -> f32 {
+                self.0 as f32
+            }
+
+            fn y_float32(&self) -> f32 {
+                self.1 as f32
+            }
         }
     };
 }
@@ -27,18 +67,57 @@ impl_grid_point_tuple!((usize, usize));
 
 // Vec2
 impl Point for Vec2 {
-    fn x(&self) -> i32 { self.x.floor() as i32 }
+    fn x_int32(&self) -> i32 {
+        self.x.floor() as i32
+    }
 
-    fn y(&self) -> i32 { self.y.floor() as i32 }
+    fn y_int32(&self) -> i32 {
+        self.y.floor() as i32
+    }
+
+    fn x_uint32(&self) -> u32 {
+        self.x.floor() as u32
+    }
+
+    fn y_uint32(&self) -> u32 {
+        self.y.floor() as u32
+    }
+
+    fn x_float32(&self) -> f32 {
+        self.x
+    }
+
+    fn y_float32(&self) -> f32 {
+        self.y
+    }
 }
 
 // (f32, f32)
 impl Point for (f32, f32) {
-    fn x(&self) -> i32 { self.0.floor() as i32 }
+    fn x_int32(&self) -> i32 {
+        self.0.floor() as i32
+    }
 
-    fn y(&self) -> i32 { self.1.floor() as i32 }
+    fn y_int32(&self) -> i32 {
+        self.1.floor() as i32
+    }
+
+    fn x_uint32(&self) -> u32 {
+        self.0.floor() as u32
+    }
+
+    fn y_uint32(&self) -> u32 {
+        self.1.floor() as u32
+    }
+
+    fn x_float32(&self) -> f32 {
+        self.0
+    }
+
+    fn y_float32(&self) -> f32 {
+        self.1
+    }
 }
-
 
 // TODO: Hide behind feature and uncomment
 // TilePos
