86bf5e6727c43c379b54960b09153a75627637d3
Wesley Feldsine <laocoon7@gmail.com>
2022-12-09 17:49:21 -0800

    shrink dimensions

--- src/types/dimensions/dimensions.rs
+++ src/types/dimensions/dimensions.rs
@@ -3,36 +3,10 @@ use crate::prelude::*;
 ////////////////////////////////////////////////////////////
 // Dimensions
 ////////////////////////////////////////////////////////////
-const MAX_SIZE: u32 = i32::MAX as u32;
-
-#[derive(Debug)]
-pub struct DimensionTooLargeForSize;
-
-const fn check_size(value: u32) -> bool {
-    value <= MAX_SIZE
-}
 
 /// A trait for types representing a 2d dimension.
 #[allow(clippy::new_ret_no_self)]
 pub trait Dimensions: Clone + Copy {
-    // Safely create a new UVec2
-    fn new_try(width: u32, height: u32) -> Option<UVec2> {
-        if check_size(width) && check_size(height) {
-            Some(UVec2::new(width, height))
-        } else {
-            None
-        }
-    }
-
-    // Create a new UVec2
-    // Panics if `width` or `height` is greater than `i32::MAX`
-    fn new(width: u32, height: u32) -> UVec2 {
-        Self::new_try(width, height).map_or_else(
-            || panic!("Size is too big: ({width}, {height}). Max is {MAX_SIZE}."),
-            |size| size,
-        )
-    }
-
     /// Returns width value.
     fn width(&self) -> u32;
 
@@ -40,7 +14,7 @@ pub trait Dimensions: Clone + Copy {
     fn height(&self) -> u32;
 
     #[inline]
-    fn count(&self) -> usize {
+    fn len(&self) -> usize {
         (self.width() * self.height()) as usize
     }
 
@@ -48,18 +22,6 @@ pub trait Dimensions: Clone + Copy {
     #[inline]
     fn as_uvec2(&self) -> UVec2 {
         UVec2::new(self.width(), self.height())
-    }
-
-    /// Convert dimensions to IVec2 (i32).
-    #[inline]
-    fn as_ivec2(&self) -> IVec2 {
-        self.as_uvec2().as_ivec2()
-    }
-
-    /// Convert dimensions to `Vec2` (f32).
-    #[inline]
-    fn as_vec2(&self) -> Vec2 {
-        self.as_uvec2().as_vec2()
     }
 
     /// Convert dimensions to `[i32;2]`.
