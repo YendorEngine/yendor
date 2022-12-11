aae8b8d8ba9621628b269de6662889c842758f40
Jacob LeCoq <lecoqjacob@gmail.com>
2022-12-10 21:31:26 -0600

    clippy

--- src/types/pathfinding/pathfinder.rs
+++ src/types/pathfinding/pathfinder.rs
@@ -11,12 +11,10 @@ impl PathFinder {
         origin: Position<GRID_WIDTH, GRID_HEIGHT>,
         destination: Position<GRID_WIDTH, GRID_HEIGHT>,
         provider: &mut impl PathProvider<T, GRID_WIDTH, GRID_HEIGHT>,
-        pass_through_data: T,
+        pass_through_data: &T,
     ) -> Vec<Position<GRID_WIDTH, GRID_HEIGHT>> {
         match self {
-            Self::Astar => {
-                AStar::compute_path(origin, destination, provider, pass_through_data)
-            },
+            Self::Astar => AStar::compute_path(origin, destination, provider, pass_through_data),
             Self::Dijkstras(partial_path) => match partial_path {
                 true => Dijkstras::compute_path_partial(origin, destination, provider, pass_through_data),
                 false => Dijkstras::compute_path(origin, destination, provider, pass_through_data),
