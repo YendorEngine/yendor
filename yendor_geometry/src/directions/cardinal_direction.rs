b92c59ad88dafadcbe35612bb2d0eb5edc155616
Jacob LeCoq <lecoqjacob@gmail.com>
2022-12-12 12:08:38 -0600

    add from_ocatant to CardinalDirection

--- yendor_geometry/src/directions/cardinal_direction.rs
+++ yendor_geometry/src/directions/cardinal_direction.rs
@@ -1,21 +1,37 @@
 // Include Private
 use super::*;
+use crate::prelude::*;
 
-/// Cardinal Directions Include (`North`, `East`, `South`, `West`)
+/// Cardinal Directions Include (`NORTH`, `EAST`, `SOUTH`, `WEST`)
 pub struct CardinalDirection;
 
 impl CardinalDirection {
-    /// Returns a [`Direction`] representing `East`
+    /// Returns a [`Direction`] representing `EAST`
     pub const EAST: Direction = Direction::EAST;
-    /// Returns a [`Direction`] representing `North`
+    /// Returns a [`Direction`] representing `NORTH`
     pub const NORTH: Direction = Direction::NORTH;
-    /// Returns a [`Direction`] representing `South`
+    /// Returns a [`Direction`] representing `SOUTH`
     pub const SOUTH: Direction = Direction::SOUTH;
-    /// Returns a [`Direction`] representing `West`
+    /// Returns a [`Direction`] representing `WEST`
     pub const WEST: Direction = Direction::WEST;
+
+    pub fn from_octant<const DIM: UVec2>(octant: Octant<DIM>) -> Direction {
+        // TODO: match on the range??
+        match octant.0 {
+            0 => Self::EAST,
+            1 => Self::NORTH,
+            2 => Self::NORTH,
+            3 => Self::WEST,
+            4 => Self::WEST,
+            5 => Self::SOUTH,
+            6 => Self::SOUTH,
+            7 => Self::EAST,
+            _ => unreachable!(),
+        }
+    }
 }
 
 impl DirectionIterator for CardinalDirection {
-    /// Returns an iterator over the [`Direction`]s (`North`, `East`, `South`, `West`)
+    /// Returns an iterator over the [`Direction`]s (`NORTH`, `EAST`, `SOUTH`, `WEST`)
     fn all() -> DirectionIter { DirectionIter::cardinal() }
 }
